070eb0e2367512a6d16485ae420d80b3760ab7f9
Cesar Cruz <cesar.cruz182@gmail.com>
2019-01-27 12:55:14 -0600

    Cipher implementation. Verified with paper's test vector

--- C/SIMON32-64_LWC/simon32-64.c
+++ C/SIMON32-64_LWC/simon32-64.c
@@ -14,17 +14,19 @@
 #define J 0
 
 int** key_expansion(int* key);
-
+int* cipher(int* plaintext, int** keys);
 int** init_keys(int* key_words);
 
 /* Auxiliar functions */
 int* xor(int* arr1, int* arr2, int size);
+int* and(int* arr1, int* arr2, int size);
 int* left_shift(int* array, int times);
 int* right_shift(int* array, int times);
 int* not(int* array, int size);
 int* z_expansion(int z, int size);
 int* copy_array(int* origin, int size);
 int* get_chunk(int* array, int begin, int final);
+int* join_chunks(int* ch1, int* ch2, int s1, int s2);
 void print_array(int*array, int size);
 
 int main() {
@@ -36,13 +38,14 @@ int main() {
   int plaintext[N * 2] = {0,1,1,0, 0,1,0,1, 0,1,1,0, 0,1,0,1,
       0,1,1,0, 1,0,0,0, 0,1,1,1, 0,1,1,1};
 
-  int** keys = key_expansion(key);
+  int* ciphertext = malloc(sizeof(int) * (2 * N));
 
-  for (int i = 0; i < T; i++) {
-    printf("%d ->", i);
-    print_array(keys[i], N);
-  }
-	
+  int** keys = key_expansion(key);
+  ciphertext = cipher(plaintext, keys);
+
+  print_array(ciphertext, N * 2);
+  
+
 }
 
 int** key_expansion(int* key) {
@@ -74,6 +77,29 @@ int** key_expansion(int* key) {
   return keys;
 }
 
+int* cipher(int* plaintext, int** keys) {
+  int* x = malloc(sizeof(int) * N);
+  int* y = malloc(sizeof(int) * N);
+  int* tmp = malloc(sizeof(int) * N);
+  int* tmp2 = malloc(sizeof(int) * N);
+  
+  /* X and Y */
+  x = get_chunk(plaintext, 0, 16);
+  y = get_chunk(plaintext, 16, 32);
+  
+  for (int i = 0; i < T; i++) {    
+    tmp = copy_array(x, N);
+    tmp2 = left_shift(x, 1);
+    tmp2 = and(tmp2, left_shift(x, 8), N);
+    tmp2 = xor(y, tmp2, N);
+    tmp2 = xor(tmp2, left_shift(x, 2), N);
+    x = xor(tmp2, keys[i], N);
+    y = copy_array(tmp, N);    
+  }
+
+  return join_chunks(x, y, N, N);
+}
+
 int** init_keys(int* key_words) {
   int** keys = (int **) malloc(sizeof(int) * 128);
 
@@ -97,21 +123,33 @@ int* xor(int* arr1, int* arr2, int size) {
   return tmp;
 }
 
+
+int* and(int* arr1, int* arr2, int size) {
+  int* tmp = malloc(sizeof(int) * size);
+  
+  for (int i = 0; i < size; i++) {    
+    tmp[i] = arr1[i] & arr2[i];
+  }
+
+  return tmp;
+}
 int* left_shift(int* array, int times) {
   int i = 0, j = 0;
+  int* tmp = malloc(sizeof(int) * N);
+  tmp = copy_array(array, N);
 
   /* Shift to the left of an array */
   for (j = 0; j < times; j++) {
-    int pos0 = array[0];
+    int pos0 = tmp[0];
     for (i = 0; i < N - 1; i++) {
-      array[i] = array[i + 1];
+      tmp[i] = tmp[i + 1];
     }
 
-    array[i] = pos0;
+    tmp[i] = pos0;
   }
   
 
-  return array;
+  return tmp;
 }
 
 int* right_shift(int* array, int times) {
@@ -119,7 +157,7 @@ int* right_shift(int* array, int times) {
   int* tmp = malloc(sizeof(int) * N);
   tmp = copy_array(array, N);
 
-  /* Shift to the left of an array */
+  /* Shift to the right of an array */
   for (j = 0; j < times; j++) {
     int posN = tmp[N - 1];
     for (i = N - 1; i > 0; i--) {
@@ -176,6 +214,20 @@ int* get_chunk(int* array, int begin, int final) {
   }  
 
   return chunk;
+}
+
+int* join_chunks(int* ch1, int* ch2, int s1, int s2) {
+  int* join = malloc(sizeof(int) * (s1 + s2));
+  
+  for (int i = 0; i < s1; i++) {
+    join[i] = ch1[i];
+  }
+
+  for (int i = s1, j = 0; i < (s1 + s2); i++, j++) {
+    join[i] = ch2[j];
+  }
+
+  return join;
 }
 
 /* Function that prints an array on account of the size */
